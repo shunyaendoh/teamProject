9424ef42232652474c19c1455ad29fb0e3db642c
shunya_endoh <shunyaEndoh.1215@gmail.com>
2019-10-02 14:12:28 +0800

    ルートの設定

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,25 @@
 Route::group(['middleware' => 'auth'], function () {
 
     // メインページ
-    Route::get('/', 'IdeaController@index')->name('idea.index');            // メインページを表示
-    Route::post('/create', 'IdeaController@store')->name('idea.store');     // アイデアを格納
+    Route::get('/', 'IdeaController@index')->name('idea.index');
 
-    // プロフィール,アイデア編集,お気に入りアイデア表示ページ
-    Route::get('/profile/{user_id}/{profile}', 'ProfileController@index')->name('profile.index');
-    Route::post('/profile/{user_id}/{profile}', 'ProfileController@create')->name('profile.create');
-    Route::get('/profile/{user_id}/{profile}', 'ProfileController@edit')->name('profile.edit');
-    Route::put('/profile/{user_id}/{profile}', 'ProfileController@update')->name('profile.update');
+    // アイデア投稿ページ
+    Route::get('/create', 'IdeaController@create')->name('idea.create');
+    Route::post('/create', 'IdeaController@store')->name('idea.store');
 
-    Route::get('/profile/{user_id}/{idea}', 'IdeaController@show')->name('idea.show');
-    Route::get('/profile/{user_id}/{idea}', 'IdeaController@edit')->name('idea.edit');
-    Route::put('/profile/{user_id}/{idea}', 'IdeaController@update')->name('idea.update');
+    // プロフィール,アイデア,お気に入りアイデア表示ページ
+    Route::get('/profile/{user}', 'ProfileController@index')->name('profile.index');
 
-    Route::get('/profile/{user_id}/{favorite}', 'FavoriteController@show')->name('favorite.show');
+    // プロフィール編集ページ
+    Route::get('/profile/edit/{user_id}', 'ProfileController@edit')->name('profile.edit');
+    Route::put('/profile/update/{user_id}', 'ProfileController@update')->name('profile.update');
+
+    // アイデア編集ページ
+    Route::get('/profile/edit/{idea}', 'IdeaController@edit')->name('idea.edit');
+    Route::put('/profile/update/{idea}', 'IdeaController@update')->name('idea.update');
 
     // チャットページ
-    ROUTE::get('/chat/{user_id}/{chat}', 'ChatController@show')->name('chat.show');
+    ROUTE::get('/chat/{user}/{chat}', 'ChatController@show')->name('chat.show');
     ROUTE::post('/chat/{user_id}/{chat}', 'ChatController@create')->name('chat.create');
     ROUTE::delete('/chat/{user_id}/{chat}', 'ChatController@delete')->name('chat.delete');
 
@@ -30,3 +32,8 @@ Route::group(['middleware' => 'auth'], function () {
 });
 
 Auth::routes();
+
+// テスト用
+Route::get('/main', function () {
+    view('app.index');
+});
