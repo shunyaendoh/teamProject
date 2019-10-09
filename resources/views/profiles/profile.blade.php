d2f3d2794eaee8f950c4e66deb240e617df04c3a
高倉楓麻 <ai24cuz@gmail.com>
2019-10-09 13:24:17 +0800

    マイページ表示機能完了

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>マイページ</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
-    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
@@ -45,10 +44,6 @@
     }
 
 
-    .right p{
-      margin-left: 20px;
-    }
-
     .left {
       display: flex;
       flex-direction: column;
@@ -63,10 +58,18 @@
       margin-bottom: 10px;
     }
 
+    .top p{
+      margin-left: 20px;
+    }
+
     .middle {
       display: flex;
       margin-bottom: 10px;
       align-items: flex-end;
+    }
+
+    .middle p{
+      margin-left: 20px;
     }
 
     .bottom {
@@ -75,6 +78,12 @@
       height: 100px;
       width: 200px;
       border: solid 1px;
+      margin-left: 20px;
+    }
+
+    .comment{
+      margin-top: 5px;
+      padding-left: 5px;
     }
 
     .name{
@@ -131,6 +140,7 @@
       background-color: #E0F2F7;
     }
 
+
     </style>
     
 </head>
@@ -142,23 +152,68 @@
                 <div class="content">
                     
                     <div class=left>
-                    <a href="IMG_0024.jpg" data-lightbox="abc" data-title="">
-                        <img src="IMG_0024.jpg" alt="プロフィール画像" class="profile">
-                    </a>
-                    <a href="#####" class= "edit">編集</a>
+                    <!-- ここにプロフィール写真入ります(class="profile") -->
+                      
+                      <a href="/{{$profile->picture_path}}" data-lightbox="abc" data-title="">
+                        <img src="/{{$profile->picture_path}}" alt="プロフィール画像" class="profile">
+                      </a>
+                      <a href="#####" class= "edit">編集</a>
                     </div>
                     
                     <div class="right">
                         <div class="top">
-                            <p class="place">関東</p>
-                            <p class="job">会社員</p>
+                            <!-- 居住地を表示 -->
+                            <p class="place">{{$profile -> locate}}</p>
+                            <!-- 職業を表示 -->
+                            <p class="job">{{$profile -> job}}</p>
+                              <!-- 性別を表示 -->
+                              @if ($profile -> gender == 1)
+                                 <!-- genderが1だった場合は男 -->
+                                <p class="gender">男性</p>
+                              @elseif ($profile -> gender == 2)
+                                 <!-- genderが2だった場合は女 -->
+                                <p class="gender">女性</p>
+                              @elseif ($profile -> gender == 0)
+                                 <!-- genderが2だった場合は女 -->
+                                <p class="gender">未設定</p>
+                              @endif
+                            
                         </div>
                         <div class="middle">
-                            <p class="name">エンチャン</p>
-                            <p Class="age">30代</p>
+                            <!-- ニックネーム表示 -->
+                            <p class="name">{{$profile -> nickname}}</p>
+                            <!-- 年齢表示 -->
+                            @if ($profile -> age < 20)
+                                 <!-- 20歳未満は10代と表示 -->
+                                <p class="age">10代</p>
+                            @elseif ($profile -> age < 30)
+                                 <!-- 30歳未満は20代と表示 -->
+                                <p class="age">20代</p>
+                            @elseif ($profile -> age < 40)
+                                 <!-- 40歳未満は30代と表示 -->
+                                <p class="age">30代</p>
+                            @elseif ($profile -> age < 50)
+                                 <!-- 50歳未満は40代と表示 -->
+                                <p class="age">40代</p>
+                            @elseif ($profile -> age < 60)
+                                 <!-- 60歳未満は50代と表示 -->
+                                <p class="age">50代</p>
+                            @elseif ($profile -> age < 70)
+                                 <!-- 70歳未満は60代と表示 -->
+                                <p class="age">60代</p>
+                            @elseif ($profile -> age < 80)
+                                 <!-- 80歳未満は70代と表示 -->
+                                <p class="age">70代</p>
+                            @elseif ($profile -> age == 0)
+                                 <!-- デフォルトで未設定 -->
+                                <p class="age">未設定</p>
+                            
+                            @endif  
+                            
                         </div>
                         <div class="bottom">
-                            しゅんや殺します！
+                            <!-- フリーコメント表示 -->
+                            <p class="comment">{{$profile -> comment}}</p>
                         </div>
                     </div>
                 </div>
