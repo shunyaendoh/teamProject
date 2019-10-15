47ccf88d867d942db02e980395aadd0665eae1e0
島田淳一 <>
2019-10-15 14:26:51 +0800

    プロフィール編集機能作りました

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -8,84 +8,80 @@
 </head>
 <body>
 <h1>プロフィールの編集</h1>
-<form>
 
-<form>
+<form action="{{ route('profile.update',['user_id' => $user->id ])}}" method="POST" name="$jobs" id="edit_profile" enctype="multipart/form-data">
+@csrf
+@method('put')
   <div class="row">
     <div class="col">
-      <input type="text" class="form-control" placeholder="First name">
-    </div>
-    <div class="col">
-      <input type="text" class="form-control" placeholder="Last name">
+      <label for="nickname">ニックネーム：</label>
+      <input type="text" class="form-control" placeholder="nickname" name="nickname">
     </div>
   </div>
-</form>
 
-<select class="custom-select">
+
+<select class="custom-select" form="edit_profile" name="age">
   <option selected>年齢</option>
-  <option value="1">20代</option>
-  <option value="2">30代</option>
-  <option value="3">40代</option>
-  <option value="4">50代</option>
-  <option value="5">60代</option>
-  <option value="6">70代</option>
+  @for($i = 0; $i < 100; $i++)
+  <option value="{{$i}}">{{$i}}</option>
+  @endfor
 </select>
+  
 
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+  <input class="form-check-input" type="radio" name="gender" id="exampleRadios1" value="1" checked>
   <label class="form-check-label" for="exampleRadios1">
     男性
   </label>
 </div>
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+  <input class="form-check-input" type="radio" name="gender" id="exampleRadios2" value="2">
   <label class="form-check-label" for="exampleRadios2">
     女性
   </label>
 </div>
 
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" disabled>
+  <input class="form-check-input" type="radio" name="gender" id="exampleRadios3" value="3">
   <label class="form-check-label" for="exampleRadios3">
     その他
   </label>
 </div>
 
-<form>
   <div class="form-row">
     <div class="col-7">
-      <input type="text" class="form-control" placeholder="City">
+      <input type="text" class="form-control" placeholder="locate" name="locate">
     </div>
     <div class="col">
-      <input type="text" class="form-control" placeholder="State">
+      <input type="text" class="form-control" placeholder="comment" name="comment">
     </div>
-    
+    <!-- <div class="col">
+      <input type="text" class="form-control" placeholder="skills" name="skills">
+    </div> -->
   </div>
-</form>
 
 
-<select class="custom-select">
+<select class="custom-select" form="edit_profile" name="job">
   <option selected>職業</option>
-  <option value="1">経営者</option>
-  <option value="2">学生</option>
-  <option value="3">会社員</option>
+  @foreach($jobs as $job)
+  <option value="{{$job->id}}">{{$job->name}}</option>
+  @endforeach
 </select>
-
+<!-- 
   <div class="form-group">
     <label for="exampleInputEmail1">メールアドレス</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" >
     <small id="emailHelp" class="form-text text-muted"></small>
   </div>
+   -->
 
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
+   <div class="form-group row">
+        <label for="picture" class="col-md-4 col-form-label text-md-right">写真</label>
+​
+        <div class="col-md-6">
+            <input id="picture" type="file" name="picture" class="form-control{{ $errors->has('picture_path') ? ' is-invalid' : '' }}">
+        </div>
+    </div>
 
   <button type="submit" class="btn btn-primary">送信</button>
 </form>
