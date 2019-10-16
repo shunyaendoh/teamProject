83707c7b08ed8c53cc673574ab64aca8b5b3da13
家田真帆 <idts-3620@yahoo.ne.jp>
2019-10-16 16:49:54 +0900

    ヘッダーフッター

--- resources/views/layouts/note.blade.php
+++ resources/views/layouts/note.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,10 +19,56 @@
     
     <header>
     
-        <div>
+        
+    <div class="collapse bg-dark" id="menu">
+                <div class="container">
+
+                  <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <!-- <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" -->
+                                    <!-- style="display: none;">
+                                    @csrf
+                                </form> -->
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+    </div> 
+
+
+
 
         <nav class="navbar navbar-light navbar-expand bg-dark d-flex justify-content-between">
-                <a class="navbar-brand" href="http://127.0.0.1:8000/">アイデア村
+                <a class="navbar-brand text-white px-3" href="http://127.0.0.1:8000/">アイデア村
                 <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true"
                             class="mr-2" viewBox="0 0 24 24" focusable="false">
@@ -31,43 +77,47 @@
                             <circle cx="12" cy="13" r="4" /></svg> -->
                 </a>
 
-
+          <div class="row">
                 <div id="menu" class="collapse">
 
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav">
 
                         <li class="nav-item">
-                            <a class="nav-link" href="">ホームページ</a>
+                            <a class="nav-link text-white" href="">ホームページ</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="">マイページ</a>
+                            <a class="nav-link text-white" href="">マイページ</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="">お問い合わせ</a>
+                            <a class="nav-link text-white" href="">お問い合わせ</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="">ログアウト</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            <a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                                ログアウト 
+                            </a>
+                            @csrf
+                        </form>
                         </li>
 
+                        
                     </ul>
-
-                    
-
-                </div>  
+          
+                </div>
                 
-                
-                
-                <button class="navbar-toggler-right" type="button" data-toggle="collapse" data-target="#menu">
+                <button class="navbar-toggler-right mx-3" type="button" data-toggle="collapse" data-target="#menu">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                    
+                
+            </div>
                     
             </nav>       
 
-        </div>
+       
 
 
 
