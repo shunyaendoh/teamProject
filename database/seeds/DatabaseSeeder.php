7fc0e8cceac16c15f01797fc51c23e8e83cff5fe
shunyaendoh1215 <shunyaendoh.1215@gmail.com>
2019-10-22 15:03:28 +0800

    delete seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(JobsTableSeeder::class);
         $this->call(UserTableSeeder::class);
         // $this->call(IdeasTableSeeder::class);
-        $this->call(FavoritesTableSeeder::class);
-        $this->call(ProfilesTableSeeder::class);
+        // $this->call(FavoritesTableSeeder::class);
+        // $this->call(ProfilesTableSeeder::class);
     }
 }
