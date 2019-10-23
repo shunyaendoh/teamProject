e2e2e3b39c2718eff487366cb30024b7bf3b6147
shunyaendoh1215 <shunyaendoh.1215@gmail.com>
2019-10-23 15:07:47 +0800

    delete job_id

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,7 +72,6 @@ class RegisterController extends Controller
             'nickname' => $data['nickname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'job_id' => 1,
         ]);
 
         $dt = Carbon::now();
